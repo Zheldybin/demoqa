7c7bb60d4445a7f1769e67db4237b44e5aa672c1
Zheldybin Anatoly <zheldybin.aa@mail.ru>
2026-02-25 13:22:22 +0300

    Added variables and improved checking.

--- src/test/java/StudentRegistrationForm.java
+++ src/test/java/StudentRegistrationForm.java
@@ -4,50 +4,44 @@ import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class StudentRegistrationForm {
     @Test
     void RegistrationForm() {
+
+        String firstName = "Jovan";
+        String lastName = "Savovich";
+        String userMail = "kgb@jovan.ru";
+        String userNumber = "4999886645";
+        String currentAddress = "Leprosorium";
+
         open("https://demoqa.com/automation-practice-form");
-        //заполнить поле first name
-        $("#firstName").setValue("Ivan");
-        //заполнить поле last name
-        $("#lastName").setValue("Ivanov");
-        //заполнить поле email
-        $("#userEmail").setValue("ivanIvanov@mail.ru");
-        //выбрать чекбокс GENDER
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userMail);
         $("#gender-radio-1").click();
-        //заполнить номер телефона
-        $("#userNumber").setValue("9501725010");
-        //выбрать дату рождения
+        $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").shouldBe(visible).selectOption("1993");
         $(".react-datepicker__month-select").shouldBe(visible).selectOption("April");
         $(".react-datepicker__day--005").click();
-        //заполнить поле subjects
         $("#subjectsInput").setValue("History");
         $(".subjects-auto-complete__menu").click();
-        //выбрать чекбокс hobbies
         $("#hobbies-checkbox-1").click();
         $("#hobbies-checkbox-3").click();
-        //загрузить фото
         File file = new File("src/test/resources/cat.jpg");
         $("#uploadPicture").uploadFile(file);
-        //заполнить поле current address
-        $("#currentAddress").setValue("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        //выбрать штаит
+        $("#currentAddress").setValue(currentAddress);
         $("#react-select-3-input").scrollIntoView(true);
         $("#react-select-3-input").click();
         $("#react-select-3-option-0").shouldBe(visible).click();
-        //выбрать город
         $("#react-select-4-input").click();
         $("#react-select-4-option-0").shouldBe(visible).click();
-        //нажать кнопку отправить
         $("#submit").click();
-        // сделать проверку
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-
-        sleep(5000);
+        $(".table-responsive").shouldHave(text(firstName), text(lastName), text(userMail), text(userNumber),
+                text("05 April,1993"), text("History"), text("cat.jpg"), text(currentAddress), text("NCR Delhi"));
     }
 }
